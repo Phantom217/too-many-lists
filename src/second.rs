21a0018acb8e6122c170b48eb224654fc51af8ea
Tyler Oalman <tyleroalman@gmail.com>
2020-01-05 15:26:57 -0600

    Implement `IterMut` for `List`

--- src/second.rs
+++ src/second.rs
@@ -15,6 +15,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -51,9 +55,15 @@ impl<T> List<T> {
     // We declare a fresh lifetime here for the *exact* borrow that
     // created the iter. Now `&self` needs to be valid as long as the
     // `Iter` is around.
-    pub fn iter(& self) -> Iter<'_, T> {
+    pub fn iter(&self) -> Iter<'_, T> {
         Iter {
             next: self.head.as_ref().map(|node| &**node),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_mut().map(|node| &mut **node),
         }
     }
 }
@@ -91,6 +101,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
             // have deref coercion applied to it.
             // self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
             &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
         })
     }
 }
@@ -175,5 +196,20 @@ mod test {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
