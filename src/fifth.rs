e8c61a966a573be6bcbc93858a27cefc6da3eb2a
Tyler Oalman <tyleroalman@gmail.com>
2020-01-10 23:49:21 -0600

    Add layout for an unsafe queue

---
+++ src/fifth.rs
@@ -0,0 +1,11 @@
+pub struct List<T> {
+    head: Link<T>,
+    tail: *mut Node<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod first;  // a bad stack
 pub mod second; // an ok stack
 pub mod third;  // a persistent stack
 pub mod fourth; // a bad but safe doubly-linked deque
+pub mod fifth;  // an unsafe singly-linked queue
